70c5f96a31fbf363afebcf193cf6e8109661816d
Ezequiel Cuñado <ecunado@gmail.com>
2016-03-02 17:24:29 +0100

    Remove promises support in data. Already supported in mongodb driver

--- test/functional/data.layer.js
+++ test/functional/data.layer.js
@@ -11,28 +11,30 @@ var Chance = require('chance');
 
 describe('API data layer', function () {
 
-  it('Should return 200 mongo connection success', function (done) {
-    zoo.api.get('/test/mongo-connection').expect(200, done);
-  });
+  describe('General', function () {
+    it('Should return 200 mongo connection success', function (done) {
+      zoo.api.get('/test/mongo-connection').expect(200, done);
+    });
 
-  it('Should return error trying to save duplicate email', function (done) {
+    it('Should return error trying to save duplicate email', function (done) {
 
-    var chance = new Chance();
-    var user = {
-      email: chance.email({
-        domain: "example.com"
-      }),
-      password: chance.string()
-    };
+      var chance = new Chance();
+      var user = {
+        email: chance.email({
+          domain: "example.com"
+        }),
+        password: chance.string()
+      };
 
-    // Create user
-    zoo.api.post('/test/user')
-      .send(user)
-      .end(function (err, res) {
-        if (err) throw err;
-        // Create user
-        zoo.api.post('/test/user').send(user).expect(400, done);
-      });
+      // Create user
+      zoo.api.post('/test/user')
+        .send(user)
+        .end(function (err, res) {
+          if (err) throw err;
+          // Create user
+          zoo.api.post('/test/user').send(user).expect(400, done);
+        });
+    });
   });
 
 });
