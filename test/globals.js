c8be68ab71a24473ac311be1832de2e710ad65ee
Ezequiel Cuñado <ecunado@gmail.com>
2016-04-02 10:46:56 +0200

    Improve globals.clone doing deep clone

--- test/globals.js
+++ test/globals.js
@@ -158,21 +158,60 @@ function createUserAndLogin(role, cb) {
  * @param  {[type]} add [description]
  * @return {[type]}     [description]
  */
-function clone(add) {
-  var origin = {};
-  // Don't do anything if add isn't an object
-  if (!add || typeof add !== 'object') return origin;
-  var keys = Object.keys(add);
-  var i = keys.length;
-  while (i--) {
+function clone(item) {
+  if (!item) {
+    return item;
+  } // null, undefined values check
+
+  var types = [Number, String, Boolean],
+    result;
+
+  // normalizing primitives if someone did new String('aaa'), or new Number('444');
+  types.forEach(function (type) {
+    if (item instanceof type) {
+      result = type(item);
+    }
+  });
+
+  if (typeof result == "undefined") {
     // Is a MongoDB ObjectID?
-    if (validations.ObjectID(add[keys[i]])) {
-      origin[keys[i]] = ObjectID.createFromHexString(add[keys[i]].toString());
+    if (validations.ObjectID(item)) {
+      result = ObjectID.createFromHexString(item.toString());
+    } else if (Object.prototype.toString.call(item) === "[object Array]") {
+      result = [];
+      item.forEach(function (child, index) {
+        result[index] = clone(child);
+      });
+    } else if (typeof item == "object") {
+      // testing that this is DOM
+      if (item.nodeType && typeof item.cloneNode == "function") {
+        result = item.cloneNode(true);
+      } else if (!item.prototype) { // check that this is a literal
+        if (item instanceof Date) {
+          result = new Date(item);
+        } else {
+          // it is an object literal
+          result = {};
+          for (var i in item) {
+            result[i] = clone(item[i]);
+          }
+        }
+      } else {
+        // depending what you would like here,
+        // just keep the reference, or create new object
+        if (false && item.constructor) {
+          // would not advice to do that, reason? Read below
+          result = new item.constructor();
+        } else {
+          result = item;
+        }
+      }
     } else {
-      origin[keys[i]] = add[keys[i]];
+      result = item;
     }
   }
-  return origin;
+
+  return result;
 }
 
 /**
